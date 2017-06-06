d7a9444023cc5f4d212b02a9602a681692643818
spidero <piotr@skwarna.eu>
2017-06-06 14:49:19 +0200

    fps info

--- first_game.py
+++ first_game.py
@@ -4,18 +4,21 @@ pygame.init()
 
 size = width, height = 640, 480
 start_poz = [5,5]
+fps_start_poz = [5,55]
 speed = [1, 1]
 left = [-1, 0]
 right = [1, 0]
 black = 0, 0, 0
 white = 255, 255, 255
 
+last_fps = 0
 point = 0
 
+c = pygame.time.Clock()
 screen = pygame.display.set_mode(size)
 
 font = pygame.font.SysFont("comicsansms", 72)
-
+fps_font = pygame.font.SysFont("comicsansms", 14)
 
 ball = pygame.image.load("ball.png")
 ballrect = ball.get_rect()
@@ -33,6 +36,12 @@ while 1:
 
     ballrect = ballrect.move(speed)
     #ballrect = ballrect.move(1,0)
+
+    c.tick()
+    if pygame.time.get_ticks() - last_fps > 1000:
+   #     print "FPS: ", c.get_fps()
+        last_fps = pygame.time.get_ticks()
+
 
     if boxrect.colliderect(ballrect):
         speed[1] = -speed[1]
@@ -56,10 +65,12 @@ while 1:
         point += 1
 
     text = font.render(str(point), True, (0, 128, 0))
+    fps_text = fps_font.render("FPS: "+str(c.get_fps()), True, (0, 128, 0))
 
     screen.fill(white)
     screen.blit(ball, ballrect)
     screen.blit(box, boxrect)
     screen.blit(text, start_poz)
+    screen.blit(fps_text, fps_start_poz)
     pygame.display.flip()
 
